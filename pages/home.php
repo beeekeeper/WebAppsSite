e514567a8262b4d7657ecf0f002337d163696f87
beeekeeper <abihunter0167@gmail.com>
2018-03-17 16:45:16 +0000

    Pretty much done with the top container
    looks wise it works, just need php and links now

--- pages/home.php
+++ pages/home.php
@@ -8,62 +8,255 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-    <!-- <link type="text/css" rel="stylesheet" href="../css/desktop.css"> -->
-    <title>Bee Keeper</title> <!-- Shows up in the tab at the top -->
+    <link type="text/css" rel="stylesheet" href="../css/desktop.css">
+    <title>TABEMASHOU</title> <!-- Shows up in the tab at the top -->
 
 <!-- Have the name of the page currently on in the header -->
+
+<style>
+ul {
+    list-style-type: none;
+    /*display: flex;
+    justify-content: space-between;
+    list-style: none;*/
+}
+
+li {
+    display: inline-block;
+    text-decoration: none;
+    min-width: 160px;
+    text-align: center;
+    cursor: pointer;
+    font-family: 'Oswald', 'PT Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif';
+    font-size: 14px;
+    font-weight : 500;
+    /*margin: 1em 0 0;*/
+    /*letter-spacing: 4px;*/
+    color: #222;
+    text-transform: uppercase;
+}
+
+li a {
+    display: block;
+    padding: 8px;
+    background-color: #FFFFFF;
+    text-decoration: none;
+    color: black;
+    font-family: 'Oswald', 'PT Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif';
+    cursor: pointer;
+}
+
+li a:hover {
+    background-color: lavender;
+}
+
+.dropBtn {
+    background-color: #FFFFFF;
+    color: black;
+    font-family: 'Oswald', 'PT Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif';
+    padding: 10px;
+    font-size: 14px;
+    border: none;
+    cursor: pointer;
+    min-width: 160px;
+    text-align: center;
+}
+
+.dropDown {
+    position: relative;
+    display: inline-block;
+}
+
+.dropContent {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    z-index: 1;
+    text-align: center;
+}
+
+.dropContent a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    font-size: 14px;
+    font-family : 'Oswald', 'PT Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif';
+}
+
+.dropContent a:hover {
+    background-color: lavender-blush;
+    color : white;
+}
+
+.dropDown:hover .dropContent {
+    display: block;
+}
+
+/*dropBtn a:hover {
+    color: purple;
+}*/
+
+.dropDown:hover .dropBtn {
+    color: lavender;
+}
+
+.topLogo {
+    font-family: 'Oswald', 'PT Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif';
+    text-decoration: none;
+    text-align: center;
+    color: black;
+    font-size: 40px;
+    font-weight: 600;
+}
+
+.topLogoUnder {
+    font-family: 'Oswald', 'PT Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif';
+    text-decoration: none;
+    text-align: center;
+    color: black;
+    font-size: 10px;
+    font-weight: 600;
+}
+
+a {
+    color: black;
+    -o-transition: .5s;
+    -ms-transition: .5s;
+    -moz-transition: .5s;
+    -webkit-transition: .5s;
+    transition: .5s;
+}
+
+a:hover {
+    color: lavender;
+    -o-transition: .5s;
+    -ms-transition: .5s;
+    -moz-transition: .5s;
+    -webkit-transition: .5s;
+    transition: .5s;
+}
+
+li:hover{
+    color: lavender;
+    -o-transition: .5s;
+    -ms-transition: .5s;
+    -moz-transition: .5s;
+    -webkit-transition: .5s;
+    transition: .5s;
+}
+
+@media (min-width: 992px) {
+    .col-md-3 {
+        width: fit-content;
+    }
+}
+
+.blankRow {
+    height: 20px !important;
+    /* overwrites any other rules */
+    background-color: #FFFFFF;
+}
+
+.blankRow2 {
+    height : 10px !important;
+    background-color : #FFFFFF;
+}
+
+.paleLine {
+    height : 2px !important;
+    background-color : #f9f9f9;
+}
+</style>
+
+
 </head>
 
 <body>
     <div class="container-fluid">
+        <div class="blankRow">
+            <div colspan="12"> <!-- Blank row -->
+            </div>
+        </div>
+    </div>
+    
+    <div class="container-fluid">
         <div class="siteContainerTop">
         <!-- This will include the Logo - and maybe the search bar and basket -->
         <a class="topLogo" href="http://homepages.shu.ac.uk/~b6042054/Abi/RecipesSite/webAppsSite/pages/home.php">
-            <span class="logoName">L O G O</span>
-        </a>
-
+        <div class="col-md-4 col-md-offset-4">
+        食べましょう
+        </div>
+        <a></a>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="siteContainerTop">
+        <!-- This will include the Logo - and maybe the search bar and basket -->
+        <a class="topLogoUnder" href="http://homepages.shu.ac.uk/~b6042054/Abi/RecipesSite/webAppsSite/pages/home.php">
+        <div class="col-md-4 col-md-offset-4">
+        - T A B E M A S H O U -
+        </div>
+        <a></a>
         </div>
     </div>
 
-<!--<div class="container-fluid">
-    <div id="navBar">
-        <ul>
-            <li><a href="#">HOME</a></li>
-        
-                <div class="dropDown">
-                    <button class="dropBtn">RECIPES</button>
-                    <div class="dropContent">
-                        <a href="#">VIEW ALL</a>
-                        <a href="#">BREAKFAST</a>
-                        <a href="#">MAIN</a>
-                        <a href="#">SIDE</a>
-                        <a href="#">SALAD</a>
-                        <a href="#">SNACK</a>
-                        <a href="#">SWEET</a>
-                        <a href="#">DRINK</a>
-                        <a href="#">COUNTRY</a>
-                        <a href="#">SPECIAL DIET</a>
-                    </div>
-                </div>
-        
-            <li><a href="#">ABOUT</a></li>
-            <li><a href="#">CONTACT</a></li>
-        </ul>
-    </div>
-</div>-->
 
 <div class="container-fluid">
+    <div class="blankRow2">
+        <div colspan="12"> <!-- Blank row -->
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
   <div class="row">
-    <div class="col-md-4">
-      One of three columns
+    <div class="col-md-2 col-md-offset-2">
+        <div class="dropDown">
+            <li class="dropBtn">H O M E</li> <!-- Add <li></li> tags to these for the css -->
+        </div>
     </div>
-    <div class="col-md-4">
-      One of three columns
+    <div class="col-md-2">
+    <div class="dropDown">
+        <li class="dropBtn">R E C I P E S</li>
+        <div class="dropContent">
+            <a href="#">V I E W  A L L</a>
+            <a href="#">B R E A K F A S T</a>
+            <a href="#">M A I N</a>
+            <a href="#">S I D E</a>
+            <a href="#">S A L A D</a>
+            <a href="#">S N A C K</a>
+            <a href="#">S W E E T</a>
+            <a href="#">D R I N K</a>
+            <a href="#">C O U N T R Y</a>
+            <a href="#">S P E C I A L - D I E T</a>
+        </div>
     </div>
-    <div class="col-md-4">
-      One of three columns
     </div>
-  </div>
+    <div class="col-md-2">
+        <div class="dropDown">
+            <li class="dropBtn">A B O U T</li>
+        </div>
+    </div>
+    <div class="col-md-2">
+        <div class="dropDown">
+            <li class="dropBtn">C O N T A C T</li>
+        </div>
+    </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="blankRow2">
+        <div colspan="12"> <!-- Blank row -->
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="paleLine">
+        <div colspan="12"> <!-- Blank row with colour to seperate the nav bar from the rest of the site body -->
+        </div>
+    </div>
 </div>
 
 </body>
